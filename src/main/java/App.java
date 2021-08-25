6121851be0a7c28916fc626b7c9abee26722ebcf
VinnieIT <mwangivin3@gmail.com>
2021-08-25 05:14:34 +0300

    Heroku deployment

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,17 +11,17 @@ import java.util.*;
 import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
-//        ProcessBuilder process = new ProcessBuilder();
-//        int port;
-//
-//        if (process.environment().get("PORT") != null) {
-//            port = Integer.parseInt(process.environment().get("PORT"));
-//        } else {
-//            port = 4567;
-//        }
-//        port(port);
+
 
 
         //get: Homepage
--- src/main/java/models/DB.java
+++ src/main/java/models/DB.java
@@ -1,10 +1,11 @@
 package models;
 
 
-import org.sql2o.*;
+import org.sql2o.Sql2o;
 
 public class DB {
-    public static Sql2o sql2o= new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","postgres","hello");
+    //public static Sql2o sql2o= new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker","postgres","hello");
+    public static Sql2o sql2o= new Sql2o("jdbc:postgresql://ec2-52-3-130-181.compute-1.amazonaws.com","bkzygooenuvxbb","365a651f45c130a0f6bb5a548cbf921330760dfab9289f9a56b5b81694baca25");
 
 }
 //    private static URI url;
